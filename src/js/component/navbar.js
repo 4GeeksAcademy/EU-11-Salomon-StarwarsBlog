6b65ad1dc4cd9d45732b6c7c8e9c65cb64db9504
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-07-04 00:44:29 +0000

    structureDeveloped

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,26 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
+		<div className="container">
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<img src="https://logospng.org/download/star-wars/star-wars-256.png"
+					className="img-thumbnail"
+					alt="..."
+					style={{ width : "30%", height: "50%" }} 
+					/>
+				</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<div className="dropdown ml-auto">
+					<Link to="/demo">
+						<a className="btn btn-primary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorites
+						</a>
+						<ul class="dropdown-menu"></ul>
+					</Link>
+				</div>
+			</nav>
 			</div>
-		</nav>
-	);
-};
+		);
+	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,195 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+	<div className="container">
+		<div className="row">
+			<h1 className="text-danger">Characters</h1>
+				<div className="card" style={{"width" : "18rem"}}>
+					<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+						<div className="card-body">
+						<h5 className="card-title">Star Wars Character</h5>
+							<p className="card-text">Gender: male <br/>
+							Hair Color: blond <br/>
+							Eye-Color: blue</p>
+							<a href="#" className="btn btn-outline-primary">Learn more!</a>
+							<a href="#" className="btn btn-outline-warning">
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+								<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<div className="card" style={{"width" : "18rem"}}>
+					<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+						<div className="card-body">
+						<h5 className="card-title">Star Wars Character</h5>
+							<p className="card-text">Gender: male <br/>
+							Hair Color: blond <br/>
+							Eye-Color: blue</p>
+							<a href="#" className="btn btn-outline-primary">Learn more!</a>
+							<a href="#" className="btn btn-outline-warning">
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+								<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<div className="card" style={{"width" : "18rem"}}>
+					<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+						<div className="card-body">
+						<h5 className="card-title">Star Wars Character</h5>
+							<p className="card-text">Gender: male <br/>
+							Hair Color: blond <br/>
+							Eye-Color: blue</p>
+							<a href="#" className="btn btn-outline-primary">Learn more!</a>
+							<a href="#" className="btn btn-outline-warning">
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+								<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<div className="card" style={{"width" : "18rem"}}>
+					<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+						<div className="card-body">
+						<h5 className="card-title">Star Wars Character</h5>
+							<p className="card-text">Gender: male <br/>
+							Hair Color: blond <br/>
+							Eye-Color: blue</p>
+							<a href="#" className="btn btn-outline-primary">Learn more!</a>
+							<a href="#" className="btn btn-outline-warning">
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+								<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<div className="row">
+					<h1 className="text-danger">Vehicles</h1>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Vehicle</h5>
+								<p className="card-text">Model: PO <br/>
+								Color: Red <br/>
+								Speed: 200km/h</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Vehicle</h5>
+								<p className="card-text">Model: PO <br/>
+								Color: Red <br/>
+								Speed: 200km/h</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Vehicle</h5>
+								<p className="card-text">Model: PO <br/>
+								Color: Red <br/>
+								Speed: 200km/h</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Vehicle</h5>
+								<p className="card-text">Model: PO <br/>
+								Color: Red <br/>
+								Speed: 200km/h</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="row">
+						<h1 className="text-danger">Planets</h1>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Planets</h5>
+								<p className="card-text">Population: 10000 <br/>
+								Terrain: Rock & Water <br/>
+								</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Planets</h5>
+								<p className="card-text">Population: 10000 <br/>
+								Terrain: Rock & Water <br/>
+								</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Planets</h5>
+								<p className="card-text">Population: 10000 <br/>
+								Terrain: Rock & Water <br/>
+								</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+						<div className="card" style={{"width" : "18rem"}}>
+						<img src="https://cdn.worldvectorlogo.com/logos/star-wars.svg" className="card-img-top" alt="..." />
+							<div className="card-body">
+							<h5 className="card-title">Star Wars Planets</h5>
+								<p className="card-text">Population: 10000 <br/>
+								Terrain: Rock & Water <br/>
+								</p>
+								<a href="#" className="btn btn-outline-primary">Learn more!</a>
+								<a href="#" className="btn btn-outline-warning">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+									<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
